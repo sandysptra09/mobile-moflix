95f3f821e330fb8e873de27f5c4399a10666cf0d
Sandy Saputra <21221753145@smkassalaambandung.sch.id>
2026-01-05 15:29:14 +0700

    feat(tabs): add and adjust tabs bottom bar with styling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ export default function Index() {
   return (
     <View
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className='text-red-500 text-center text-2xl'>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
