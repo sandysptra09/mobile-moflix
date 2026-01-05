49691e20fdd02ecffc19125bc70e0c97aa6407a1
Sandy Saputra <21221753145@smkassalaambandung.sch.id>
2026-01-05 15:54:22 +0700

    feat(home): add initalize home screen in home tabs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,39 @@
-import { Text, View } from 'react-native';
+import SearchBar from '@/components/search-bar';
+import { icons } from '@/constants/icons';
+import { images } from '@/constants/images';
+import { useRouter } from 'expo-router';
+import { Image, ScrollView, View } from 'react-native';
 
 export default function Index() {
+
+  const router = useRouter();
+
   return (
-    <View
-    >
-      <Text className='text-red-500 text-center text-2xl'>Edit app/index.tsx to edit this screen.</Text>
+    <View className='flex-1 bg-primary'>
+      <Image
+        source={images.bg}
+        className='absolute w-full z-0'
+        resizeMode='cover'
+      />
+
+      <ScrollView
+        className='flex-1 px-5'
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: '100%', paddingBottom: 10 }}
+      >
+        <Image
+          source={icons.logo}
+          className='w-12 h-10 mt-20 mb-5 mx-auto'
+        />
+
+        <View className='flex-1 mt-5'>
+          <SearchBar
+            onPress={() => router.push('/search')}
+            placeholder='Search for a movie'
+          />
+        </View>
+
+      </ScrollView>
     </View>
   );
 }
